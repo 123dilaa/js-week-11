c52e9c7b84195096fdabe190ee4685212537923c
123dilaa <145972818+123dilaa@users.noreply.github.com>
2023-12-18 12:14:28 +0700

    Add files via upload

---
+++ latihan.js
@@ -0,0 +1,58 @@
+// ini variabel dengan let
+let nama = "siti fadila siregar"
+console.log(nama)
+
+//ini variabel dengan const
+const FirstName = "siti Fadila Siregar"
+const lastname = "dilaa"
+console.log(FirstName + " " + lastname)
+
+//tipe data string
+const gender ="LK";
+console.log(typeof gender) 
+
+//ini tipe data intreger
+const age = 21
+console.log(typeof age)
+
+//tipe data boolean
+const isMarried = false;
+console.log(isMarried)
+console.log(typeof isMarried)
+
+//Operator Aritmatika
+const x = 9
+const y = 10
+
+console.log(z)
+
+//pertambahan
+const z = x - y
+
+//Pengurangan
+const m = x - y
+
+// Perkalian
+const n = x * y
+
+//Pembagian
+const c = x / y
+
+console.log(z)
+console.log(m)
+console.log(n)
+console.log(c)
+
+//fuction
+function salam() {
+    alert("assalamualaikum")
+}
+
+
+salam(); 
+
+function isTrue() {
+    let.city = "Depok"
+}
+console.log(city)
+isTrue();
